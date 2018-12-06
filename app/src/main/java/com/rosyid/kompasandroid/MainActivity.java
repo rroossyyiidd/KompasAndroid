7686c1d32f72203b9eeaed655f4f7af4f56e88b0
Rsyd <rroossyyiidd@gmail.com>
2018-12-06 14:16:21 +0700

    update

--- app/src/main/java/com/rosyid/kompasandroid/MainActivity.java
+++ app/src/main/java/com/rosyid/kompasandroid/MainActivity.java
@@ -1,14 +1,19 @@
 package com.rosyid.kompasandroid;
 
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -27,19 +32,86 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
 
         view = findViewById(R.id.txtShake);
-    }
+        view = view.getRootView();
+        Random random = new Random();
+        int color = Color.argb(255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
+        view.setBackgroundColor(color);
 
-    public void changeMode(View view) {
-
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        lastUpdate = System.currentTimeMillis();
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-
+        getAccelerometer(event);
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
+
+    private void getAccelerometer(SensorEvent event) {
+        float[] values = event.values;
+        float x = values[0];
+        float y = values[1];
+        float z = values[2];
+
+        float accelerationSquareRoot = (x * x + y * y + z + z) / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+        long actualTime = event.timestamp;
+        if (accelerationSquareRoot >= 2) {
+            if (actualTime - lastUpdate < 200) {
+                return;
+            }
+            lastUpdate = actualTime;
+            Random random = new Random();
+            int color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
+            view.setBackgroundColor(color);
+
+            Toast.makeText(getApplicationContext(), "Device was shuffled!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+        sensorManager.unregisterListener(mySensorEventListener);
+
+    }
+
+    public void changeMode(View view) {
+        sensorManager.unregisterListener(this);
+        compassView = new MyCompassView(this);
+        setContentView(compassView);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+        if (sensor != null) {
+            sensorManager.registerListener(mySensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+            Log.i("Compass Sensor Activity", "Registered for Orientation Sensor");
+        } else {
+            Log.e("Compass Sensor Activity", "Registered for Orientation Sensor");
+            finish();
+        }
+
+    }
+
+    private SensorEventListener mySensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float azimuth = event.values[0];
+            compassView.updateData(azimuth);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
+
 }
